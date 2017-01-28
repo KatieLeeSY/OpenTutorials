e5bd47e7e38e586a78ffe8f6c661dc9865d514c9
KatieLeeSY <katie0205@naver.com>
2017-01-28 12:41:56 +0900

    로그인 기능 만들기

---
+++ CodingEverybody/MakeWebApp/PHPJS/08_1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="08_2.php">
+      <p>비밀번호를 입력해주세요. </p>
+      <input type="text" name="password">
+      <input type="submit">
+  </body>
+</html>
