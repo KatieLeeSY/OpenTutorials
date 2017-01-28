e931d8bbd9bd84d5dc522fe2fa3e86be130df4b3
KatieLeeSY <katie0205@naver.com>
2017-01-28 12:00:56 +0900

    웹페이지에 코드 삽입하기

---
+++ CodingEverybody/MakeWebApp/PHPJS/01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>php</h1>
+    <?php //php
+    echo 10+10;
+    ?>
+    <h1>JavaScript</h1>
+    <script> //자바스크립트
+      document.write(10+10);
+    </script>
+  </body>
+</html>
